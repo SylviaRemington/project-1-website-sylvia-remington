bb03df8509c93b05f6407e5fabd01beacac1fa94
Sylvia Remington <sylviaremington@gmail.com>
2025-03-11 11:01:46 -0700

    adding h1 and h2 content plus random js code to try and figure out event listeners

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Select all nav links
-    const navLinks = document.querySelectorAll(".navigation-bar a");
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Select all nav links
+//     const navLinks = document.querySelectorAll(".navigation-bar a");
 
 
-    navLinks.forEach(link => {
-        link.addEventListener("click", (event) => {
-            event.preventDefault(); // Prevent default navigation
-            const pageName = link.textContent.trim(); // Get link text
-            if (pages[pageName]) {
-                window.open(pages[pageName], "_blank"); // Open in a new tab
+//     navLinks.forEach(link => {
+//         link.addEventListener("click", (event) => {
+//             event.preventDefault(); // Prevent default navigation
+//             const pageName = link.textContent.trim(); // Get link text
+//             if (pages[pageName]) {
+//                 window.open(pages[pageName], "_blank"); // Open in a new tab
